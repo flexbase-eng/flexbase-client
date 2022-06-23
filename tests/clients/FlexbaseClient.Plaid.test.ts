9283e03d2e139ce37018c7c12e5e720fbb9a9097
Robert Dean <99672719+robert-dean@users.noreply.github.com>
2022-06-23 10:00:14 -0400

    adding some documentation

--- tests/clients/FlexbaseClient.Plaid.test.ts
+++ tests/clients/FlexbaseClient.Plaid.test.ts
@@ -1,4 +1,6 @@
 import { badPlaidPublicToken, goodPlaidPublicToken, plaidLinkToken } from '../mocks/server/constants';
+import { plaid_failure_handlers, plaid_http_error_handlers } from '../mocks/server/handlers/plaid';
+import { server } from '../mocks/server/server';
 import { testFlexbaseClient } from '../mocks/TestFlexbaseClient';
 
 test("FlexbaseClient plaid link token success", async () => {
@@ -6,6 +8,24 @@ test("FlexbaseClient plaid link token success", async () => {
     const response = await testFlexbaseClient.getPlaidLinkToken();
 
     expect(response).toBe(plaidLinkToken);
+});
+
+test("FlexbaseClient plaid link token failure", async () => {
+
+    server.use(...plaid_failure_handlers);
+
+    const response = await testFlexbaseClient.getPlaidLinkToken();
+
+    expect(response).toBeNull();
+});
+
+test("FlexbaseClient plaid link token error", async () => {
+
+    server.use(...plaid_http_error_handlers);
+
+    const response = await testFlexbaseClient.getPlaidLinkToken();
+
+    expect(response).toBeNull();
 });
 
 test("FlexbaseClient exchange plaid public token success", async () => {
@@ -34,4 +54,22 @@ test("FlexbaseClient update plaid link token success", async () => {
     const response = await testFlexbaseClient.updatePlaidLinkToken();
 
     expect(response).toBe(plaidLinkToken);
+});
+
+test("FlexbaseClient update plaid link token faillure", async () => {
+
+    server.use(...plaid_failure_handlers);
+
+    const response = await testFlexbaseClient.updatePlaidLinkToken();
+
+    expect(response).toBeNull();
+});
+
+test("FlexbaseClient update plaid link token error", async () => {
+
+    server.use(...plaid_http_error_handlers);
+
+    const response = await testFlexbaseClient.updatePlaidLinkToken();
+
+    expect(response).toBeNull();
 });
