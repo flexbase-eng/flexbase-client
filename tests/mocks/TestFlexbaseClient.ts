12c18ec128d34768f4bffb368a5f5a8c01e489fd
Robert Dean <robert@flexbase.app>
2022-06-23 12:00:38 -0400

    adding decode token

--- tests/mocks/TestFlexbaseClient.ts
+++ tests/mocks/TestFlexbaseClient.ts
@@ -3,13 +3,33 @@ import { FlexbaseAuthenticationTokenAccessor, FlexbaseClient } from "../../src";
 import { mockUrl } from "./server/constants";
 import wretch from "wretch";
 import { AuthenticationToken, AuthenticationTokenAccessor } from "@flexbase/http-client-middleware";
+import { Logger } from "@flexbase/logger";
 
 export const testTokenAccessor = new Mock<FlexbaseAuthenticationTokenAccessor>();
+
+class NullLogger implements Logger{
+    error(message: any, ...optionalParams: any[]): void {
+        
+    }
+    warn(message: any, ...optionalParams: any[]): void {
+        
+    }
+    info(message: any, ...optionalParams: any[]): void {
+       
+    }
+    debug(message: any, ...optionalParams: any[]): void {
+       
+    }
+    trace(message: any, ...optionalParams: any[]): void {
+       
+    }
+
+}
 
 export const testFlexbaseClient = new FlexbaseClient(
     wretch(mockUrl)
         .polyfills({ fetch: require("node-fetch") }),
-    testTokenAccessor.object());
+    testTokenAccessor.object(), new NullLogger());
 
 export class ProtectedFlexbaseClient extends FlexbaseClient {
     setAuthenticationToken(authToken: AuthenticationToken | null) {
