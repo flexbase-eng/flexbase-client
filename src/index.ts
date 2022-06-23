89618bdce44a648837b7b4f98a7211faa51fbf93
Robert Dean <robert@flexbase.app>
2022-06-23 13:04:16 -0400

    update import/exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
-export { FlexbaseClient } from './clients/FlexbaseClient';
-export { FlexbaseAuthenticationTokenAccessor } from './auth/FlexbaseAuthenticationTokenAccessor';
-export { convertFlexbaseToken } from './auth/ConvertFlexbaseToken';
-export { Address } from './models/Address';
-export { FlexbasePasswordCredentials } from './models/FlexbasePasswordCredentials';
-export { Invoice } from './models/Invoice';
-export { decodeFlexbaseToken } from './auth/DecodeFlexbaseToken';
+import { FlexbaseClient } from './clients/FlexbaseClient';
+import { FlexbaseAuthenticationTokenAccessor } from './auth/FlexbaseAuthenticationTokenAccessor';
+import { convertFlexbaseToken } from './auth/ConvertFlexbaseToken';
+import { Address } from './models/Address';
+import { FlexbasePasswordCredentials } from './models/FlexbasePasswordCredentials';
+import { Invoice } from './models/Invoice';
+import { decodeFlexbaseToken } from './auth/DecodeFlexbaseToken';
+
+export {
+    FlexbaseClient,
+    FlexbaseAuthenticationTokenAccessor,
+    convertFlexbaseToken,
+    Address,
+    FlexbasePasswordCredentials,
+    Invoice,
+    decodeFlexbaseToken
+}
