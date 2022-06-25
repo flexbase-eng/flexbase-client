b52cf935124b964d18a0f121cd2c85713b580f59
Robert Dean <robert@flexbase.app>
2022-06-25 10:21:01 -0400

    fixed rollup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 export { FlexbaseClient } from './clients/FlexbaseClient';
 export { FlexbaseAuthenticationTokenAccessor } from './auth/FlexbaseAuthenticationTokenAccessor';
 export { convertFlexbaseToken } from './auth/ConvertFlexbaseToken';
-export type { Address } from './models/Address';
+export { Address } from './models/Address';
 export { FlexbasePasswordCredentials } from './models/FlexbasePasswordCredentials';
-export type { Invoice } from './models/Invoice';
+export { Invoice } from './models/Invoice';
 export { decodeFlexbaseToken } from './auth/DecodeFlexbaseToken';
+export { FlexbaseTokenResponse } from './models/FlexbaseTokenResponse';
