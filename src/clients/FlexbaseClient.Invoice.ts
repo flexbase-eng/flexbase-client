2f8d85fb032cd19d5ae05d00f0136e768fe22c41
alejandra@edukathe.org <alejandra@edukathe.org>
2022-07-06 11:52:47 -0500

    Add uploadInvoiceFile and updateInvoice calls

--- src/clients/FlexbaseClient.Invoice.ts
+++ src/clients/FlexbaseClient.Invoice.ts
@@ -2,11 +2,22 @@ import { InvoiceOptions } from '../models/Invoice/InvoiceOptions';
 import { Invoice } from '../models/Invoice/Invoice';
 import { FlexbaseClientBase } from './FlexbaseClient.Base';
 
-interface InvoiceResponse {
+interface InvoicesResponse {
     success: boolean;
     error?: string;
     invoices: Invoice[];
 }
+
+interface InvoiceResponse {
+    error: string;
+    success: boolean;
+    invoice: Invoice;
+  }
+
+interface InvoiceForm {
+    contractId: string;
+    description: string;
+  }
 
 export class FlexbaseClientInvoice extends FlexbaseClientBase {
     private buildParams(options?: InvoiceOptions) {
@@ -44,7 +55,7 @@ export class FlexbaseClientInvoice extends FlexbaseClientBase {
         try {
             const params = this.buildParams(options);
 
-            const response = await this.client.url(`/invoice/company/${companyId}`).query(params).get().json<InvoiceResponse>();
+            const response = await this.client.url(`/invoice/company/${companyId}`).query(params).get().json<InvoicesResponse>();
 
             if (!response.success) {
                 return null;
@@ -61,7 +72,7 @@ export class FlexbaseClientInvoice extends FlexbaseClientBase {
         try {
             const params = this.buildParams(options);
 
-            const response = await this.client.url(`/invoice/user/${userId}`).query(params).get().json<InvoiceResponse>();
+            const response = await this.client.url(`/invoice/user/${userId}`).query(params).get().json<InvoicesResponse>();
 
             if (!response.success) {
                 return null;
@@ -73,4 +84,34 @@ export class FlexbaseClientInvoice extends FlexbaseClientBase {
             return null;
         }
     }
+
+    async uploadInvoiceFile(file: File, invoiceId: string): Promise<Invoice[] | null> {
+        try {
+          const response = await this.client.url(`/invoice/${invoiceId}/invoicePic`).formData({ file }).post().json();
+         
+            if (!response.success) {
+              return null;
+            }
+
+           return response.invoice;
+        } catch (error) {
+            this.logger.error('Unable to upload invoice file', error);
+            return null;
+        }
+      }
+    
+      async updateInvoice(invoiceId: string, invoiceForm: InvoiceForm): Promise<Invoice | null> {
+        try {
+          const response = await this.client.url(`/invoice/${invoiceId}/summary`).put(invoiceForm).json<InvoiceResponse>();
+            
+            if (!response.success) {
+                return null;
+            }
+  
+           return response.invoice;
+        } catch (error) {
+          console.error('Unable to update the invoice', error);
+          return null;
+        }
+      }
 }
