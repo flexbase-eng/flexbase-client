e4206bf3adfe2f4cade036d6b06c948a33b49f78
Andres <andrexxperex@gmail.com>
2022-07-06 15:30:40 -0500

    create addPatron and updatePatron functions

--- src/clients/FlexbaseClient.Patron.ts
+++ src/clients/FlexbaseClient.Patron.ts
@@ -1,6 +1,21 @@
 import { Patron } from '../models/Patron/Patron';
 import { FlexbaseClientBase } from './FlexbaseClient.Base';
 
+interface IPatronData {
+    id?: string;
+    name?: string;
+    address?: string;
+    state?: string;
+    city?: string;
+    postalCode?: string;
+  }
+
+interface IResponseUpdatePatron {
+    patron?: Patron;
+    success: boolean;
+    message?: string;
+  }
+
 export class FlexbaseClientPatron extends FlexbaseClientBase {
     async getPatrons(patronId?: string): Promise<Patron[]> {
         try {
@@ -16,4 +31,68 @@ export class FlexbaseClientPatron extends FlexbaseClientBase {
             return [];
         }
     }
+
+    async addPatron(patronData: IPatronData): Promise<IResponseUpdatePatron> {
+        try {
+          const response = await this.client
+            .url('/clients')
+            .post(patronData)
+            .json();
+    
+          return {
+            patron: {
+              id: response.id,
+              name: response.name,
+              companyId: response.companyId,
+              tenantId: response.tenantId,
+              address: {
+                street1: response.address,
+                city: response.city,
+                country: response.country,
+                state: response.state,
+                postalCode: response.postalCode,
+              }
+            },
+            success: true,
+            message: 'You have a new patron!',
+          };
+        } catch (error) {
+            this.logger.error(`Unable to add patron`, error);
+            return { success: false, message: 'Unable to add patron' }
+        }
+      }
+
+    async updatePatron(patronData: IPatronData): Promise<IResponseUpdatePatron> {
+        try {
+          if (patronData?.id) {
+            const response = await this.client
+              .url('/clients')
+              .post(patronData)
+              .json();
+    
+            return {
+              patron: {
+                id: response.id,
+                name: response.name,
+                companyId: response.companyId,
+                tenantId: response.tenantId,
+                address: {
+                    street1: response.address,
+                    city: response.city,
+                    country: response.country,
+                    state: response.state,
+                    postalCode: response.postalCode,
+                }
+              },
+              success: true,
+              message: 'The information has been updated!',
+            };
+          }
+          return { success: false, message: 'Unable to update client data' };
+        } catch (error) {
+          this.logger.error(`Unable to update patron`, error);
+          return { success: false, message: 'Unable to update patron' }
+        }
+      }
+
 }
