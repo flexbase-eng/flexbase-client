3a112c1202033ab12cb3cff63712afd16eda481e
Mariana0711 <mariana@edukathe.org>
2022-07-11 13:10:34 -0500

    Add the address information and change the function name to getEmployees

--- src/models/Company/Employee.ts
+++ src/models/Company/Employee.ts
@@ -1,8 +1,12 @@
-export default interface IEmployee {
+import { Address } from '../Address/Address';
+
+export interface Employee {
+    id?: string;
     firstName?: string;
     lastName?: string;
+    birthDate?: string;
     jobTitle?: string;
     email?: string;
+    address?: Address;
     phone?: string;
-    id?: string;
-  }
+}
