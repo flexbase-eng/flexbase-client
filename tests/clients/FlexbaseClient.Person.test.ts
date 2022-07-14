1c11cca35d1c834a4e5d266381381f48ab4eb663
Mariana0711 <mariana@edukathe.org>
2022-07-14 11:13:32 -0500

    Delete the employees client and add the function to the persons client

--- tests/clients/FlexbaseClient.Person.test.ts
+++ tests/clients/FlexbaseClient.Person.test.ts
@@ -1,5 +1,32 @@
 import { badUserId, errorUserId, goodUserId } from "../mocks/server/constants";
+import { employees_error_handlers } from '../mocks/server/handlers/employees';
+import { server } from '../mocks/server/server';
 import { testFlexbaseClient } from "../mocks/TestFlexbaseClient";
+
+
+test("FlexbaseClient get employees success", async () => {
+
+    const response = await testFlexbaseClient.getEmployees();
+
+    expect(response).not.toBeNull();
+    expect(response.employees?.length).toBeGreaterThan(0);
+
+    const employee = response[0];
+    expect(employee.id).toBe(goodUserId);
+    expect(employee.firstName).toBe("Ann");
+    expect(employee.jobTitle).toBe("Manager");
+
+});
+
+test("FlexbaseClient get employees error", async () => {
+
+    server.use(...employees_error_handlers);
+
+    const response = await testFlexbaseClient.getEmployees();
+
+    expect(response).not.toBeNull();
+    expect(response.employees?.length).toBe(0);
+});
 
 test("FlexbaseClient get person success", async () => {
 
