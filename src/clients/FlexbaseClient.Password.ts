dbc5a01f78ec046ce9414a21f0d1c1bf4ec22b58
Andres <andrexxperex@gmail.com>
2022-08-03 19:21:58 -0500

    create validatePassword function

--- src/clients/FlexbaseClient.Password.ts
+++ src/clients/FlexbaseClient.Password.ts
@@ -1,6 +1,11 @@
 import { FlexbaseResponse } from '../models/FlexbaseResponse';
 import { FlexbaseClientBase } from './FlexbaseClient.Base';
 
+interface validatePassResponse {
+    token: string | null;
+    success?: boolean;
+    error?: string;
+}
 
 export class FlexbaseClientPassword extends FlexbaseClientBase {
     async changePassword(password: string): Promise<FlexbaseResponse> {
@@ -18,6 +23,24 @@ export class FlexbaseClientPassword extends FlexbaseClientBase {
         } catch (error) {
             this.logger.error(`Unable to change the password`, error);
             return { success: false, error: 'Unable to change the password' };
+        }
+    }
+
+    async validatePassword(email: string, password: string): Promise<validatePassResponse> {
+        try {
+            const response = await this.client
+                .url(`/auth/token`)
+                .post({ email, password })
+                .json<validatePassResponse>();
+
+            if (!response.success) {
+                this.logger.error(`failed to validate password`, response.error);
+            }
+
+            return  response;
+        } catch (error) {
+            this.logger.error(`failed to validate password`, error);
+            return { success: false, error: 'failed to validate password', token: null };
         }
     }
 }
--- tests/clients/FlexbaseClient.Password.test.ts
+++ tests/clients/FlexbaseClient.Password.test.ts
@@ -21,3 +21,22 @@ test("FlexbaseClient change password failure", async () => {
     expect(response.success).toBe(false);
     expect(response.error).toBe('Unable to change the password');
 });
+
+test("FlexbaseClient validate password success", async () => {
+
+    const response = await testFlexbaseClient.validatePassword("good@email.com", "Flex1234**");
+
+    expect(response).not.toBeNull();
+
+    expect(response.success).toBe(true);
+
+});
+
+test("FlexbaseClient validate password failure", async () => {
+
+    server.use(...password_failure_handlers);
+
+    const response = await testFlexbaseClient.validatePassword('', '');
+
+    expect(response.token).toBe(null);
+});
