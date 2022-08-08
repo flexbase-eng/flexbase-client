d546fc4e80de02b7e01e51252dec163f4fd1d928
Alejandra González <79479443+alegoos@users.noreply.github.com>
2022-08-08 15:27:18 -0500

    Banking statements (#28)
    * Create getBankingStatements() to get the list of statements or a specific statement
    
    * Add params
    * Create test
    * Clean code
    * Export statement model
    * Add 'roles' value on Person interface
    * Fix interface error

---
+++ src/models/Banking/Statement.ts
@@ -0,0 +1,19 @@
+export interface Relationship {
+    data: {
+      id: string;
+      type: string;
+    };
+}
+
+export interface Statement {
+    id: string;
+    type: "statement";
+    attributes: {
+        period: string;
+    };
+    relationships: {
+        account: Relationship;
+        customer?: Relationship;
+        customers?: Relationship[];
+    }
+}
--- tests/mocks/server/handlers/banking.ts
+++ tests/mocks/server/handlers/banking.ts
@@ -69,4 +69,75 @@ export const banking_handlers = [
         );
         return response(res);
     }),
+
+    mockServer.get(mockUrl + "/banking/:companyId/statements", (request, response, context) => {
+
+        const { companyId } = request.params;
+
+        if (!companyId || companyId === errorCompanyId) {
+            const res = compose(
+                context.status(400),
+            );
+            return response(res);
+        }
+
+        else if (companyId === badCompanyId) {
+            const res = compose(
+                context.status(200),
+                context.json({
+                    success: false,
+                    error: 'Unable to get the list of statements',
+                })
+            );
+            return response(res);
+        }
+
+        const res = compose(
+            context.status(200),
+            context.json({
+                statement: [
+                    {
+                        id: '0123',
+                        type: "statement",
+                    }
+                ],
+                success: true,
+            }),
+
+        );
+        return response(res);
+    }),
+
+    mockServer.get(mockUrl + "/banking/:companyId/statements/:statementId", (request, response, context) => {
+
+        const { companyId } = request.params;
+
+        if (!companyId || companyId === errorCompanyId) {
+            const res = compose(
+                context.status(400),
+            );
+            return response(res);
+        }
+
+        else if (companyId === badCompanyId) {
+            const res = compose(
+                context.status(200),
+                context.json({
+                    success: false,
+                    error: 'Unable to get the statement details for statementId 0123',
+                })
+            );
+            return response(res);
+        }
+
+        const res = compose(
+            context.status(200),
+            context.json({
+                statement: 'html/pdf document',
+                success: true,
+            }),
+
+        );
+        return response(res);
+    }),
 ]
