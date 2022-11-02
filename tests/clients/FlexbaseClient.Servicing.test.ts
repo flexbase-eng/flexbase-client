4e785ffa0328d1f2bdf658db85915046731a81e8
Andres <andrexxperex@gmail.com>
2022-11-02 14:29:26 -0500

    add origin to the interface InvoiceNew

--- tests/clients/FlexbaseClient.Servicing.test.ts
+++ tests/clients/FlexbaseClient.Servicing.test.ts
@@ -6,9 +6,12 @@ test("FlexbaseClient get credit statement data", async () => {
     const response = await testFlexbaseClient.getCreditStatement(goodCompanyId, { target: '2022-09-15' });
     expect(response).not.toBeNull();
 
+    const invoicesNew = response?.invoicesNew || [];
+
     expect(response?.success).toBeTruthy();
     expect(response?.invoicesNewPeriodFrom).toBe("2022-08-16");
     expect(response?.invoicesNewPeriodTo).toBe("2022-08-31");
+    expect(invoicesNew[0]?.origin).toBe("bnpl");
     expect(response?.company?.active).toBeTruthy();
     expect(response?.company?.address).toBe("5018 Bridgevalley Ct");
     expect(response?.company?.name).toBe("Texas Stag Roofing Solutions");
